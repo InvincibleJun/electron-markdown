0c78489b6123c228ec66f63497b4a4446dc3557c
chenjun <398955404@qq.com>
2018-01-10 14:04:55 +0800

    merge

--- main.js
+++ main.js
@@ -17,12 +17,12 @@ function createWindow () {
   mainWindow = new BrowserWindow({width: 800, height: 600})
 
   // and load the index.html of the app.
-  // mainWindow.loadURL(url.format({
-  //   pathname: path.join(__dirname, './build/index.html'),
-  //   protocol: 'file:',
-  //   slashes: true
-  // }))
-  mainWindow.loadURL('http://localhost:3000/')
+  mainWindow.loadURL(url.format({
+    pathname: path.join(__dirname, './build/index.html'),
+    protocol: 'file:',
+    slashes: true
+  }))
+  // mainWindow.loadURL('http://localhost:3000/')
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
